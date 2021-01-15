e21ce080d087adc1041596865325564b0a926093
guilhermediniz3 <gdiniztec@yahoo.com.br>
2021-01-15 14:31:37 -0200

    testando

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,11 +1,11 @@
 package com.nelioalves.cursomc.services;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
-import com.sun.el.stream.Optional;
 
 @Service
 public class CategoriaService {
